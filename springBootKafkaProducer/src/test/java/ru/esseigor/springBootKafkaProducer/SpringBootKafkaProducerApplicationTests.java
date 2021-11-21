dfa9a50ab56bea1c86dfeff3f7be8e7591db2136
IgorEsse <esseigor@gmail.com>
2021-11-21 13:29:27 +0400

    Init commit

---
+++ springBootKafkaProducer/src/test/java/ru/esseigor/springBootKafkaProducer/SpringBootKafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.esseigor.springBootKafkaProducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
